2a15ea65244b1bf354551d761d8422b0fead73fc
Joy Taylor <jcat7414@gmail.com>
2021-10-11 16:29:22 +1000

    Date fields to automatically fill with today's date

--- sweetshot/memory/models.py
+++ sweetshot/memory/models.py
@@ -2,13 +2,13 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 class Memory(models.Model):
-    date_created = models.DateField()
+    date_created = models.DateField(auto_now_add=True)
     club = models.CharField(max_length=100)
     course = models.TextField()
     shot_date = models.DateField()
     weather = models.TextField()
     memory_details = models.TextField(max_length=5000)
-    date_amended = models.DateField()
+    date_amended = models.DateField(auto_now_add=True)
     is_current = models.BooleanField()
     owner = models.ForeignKey(
         get_user_model(),
